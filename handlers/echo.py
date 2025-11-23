3c512d782bbd4bdffd3a14fcc1212adb2352d5a4
StandoffKitty75 <standoffkitty75@gmail.com>
2025-11-23 22:07:49 +0300

    Some changes

--- handlers/echo.py
+++ handlers/echo.py
@@ -1,13 +1,9 @@
 from aiogram import Router, types
 from aiogram.filters import Command
+from aiogram.types import Message
 
 router = Router()
 
-@router.message(Command("echo"))
-async def echo_command(message: types.Message):
-    """Простая команда, которая выводит текст"""
-    await message.answer("📢 Это тестовый текст от бота!")
-
-# Если у вас принято регистрировать хендлеры через функцию
-def register_handlers(router: Router):
-    router.message.register(echo_command, Command("echo"))
+@router.message(Command("circle"))
+async def cmd_circle(message: Message):
+    await message.answer("Теперь пришли мне видео, и я сделаю из него кружочек 🎥")
